f158e830d0340e9b37afdf7ed48e2bb7f08fe9a2
RedDaXe <syammalikmn@gmail.com>
2018-08-10 20:52:32 +0800

    Third commit

---
+++ Rectangle.py
@@ -0,0 +1,8 @@
+print "Calculate Area & Volume of a rectangle"
+height = input("Please enter height : ")
+width = input ("Please enter width : ")
+length = input ("Please enter length : ")
+print "Area for rectangle is : ", width * length
+print "Volume for rectangle is : ", height * length * width
+
+raw_input("Press any key to escape")
